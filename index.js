b5734d0e240c46ed57219eb055042e12eab3657a
unknown <valinfernanda08@gmail.com>
2026-01-20 21:04:36 +0100

    Update yml

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World, from simple JS app!\n');
+  res.end('Hello World, from simple JS app!!\n');
 });
 
 server.listen(3000, () => {
